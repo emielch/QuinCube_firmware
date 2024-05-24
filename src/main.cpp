9a2bcc1e075c768558b7b570e355366506ba5e49
Emiel Harmsen <emielch@gmail.com>
2024-05-24 16:12:14 +0200

    switch to Teensy 4.1 using CubeOS and ShiftWS2811

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,19 @@
 #include <Arduino.h>
-#include <EEPROM.h>
 
-#include "DemoManager.h"
-#include "LedManager.h"
-#include "SerialStreamManager.h"
-#include "TimeManager.h"
-#include "Touch/MPR121_Manager.h"
-#include "Touch/TouchbarManager.h"
-
-void renderInterrupt() {
-  mpr121Manager.update();
-}
-
-unsigned long randomSeedVar = 0;
-void exciteRandomizer() {
-  EEPROM.get(0, randomSeedVar);
-  randomSeedVar++;
-  EEPROM.put(0, randomSeedVar);
-  randomSeed(randomSeedVar);
-}
+#include "CubeOS\Audio\AudioManager.h"
+#include "CubeOS\Demo\DemoManager.h"
+#include "CubeOS\Driver.h"
+#include "CubeOS\SerialStreamManager.h"
 
 void setup() {
-  //while(!Serial){}
-  exciteRandomizer();
+  cube->init();
   serialStreamManager.init();
-  mpr121Manager.init();
-  touchbarManager.init();
-  ledManager.init();
-  demoManager.init(&renderInterrupt);
-
-  //Teensy3Clock.set(1713889270);  // set the time manually, auto set during compile time doesn't seem to work...
+  audioManager.init();
+  demoManager.init();
 }
 
 void loop() {
-  timeManager.update();
   serialStreamManager.update();
-  mpr121Manager.update();  // also called from within the animations through renderInterrupt() for higher sample rate
-  touchbarManager.update();
+  audioManager.update();
   demoManager.update();
 }
