87ec4272bebd868cf0bd0277b815cb83af490e93
Emiel Harmsen <emielch@gmail.com>
2024-05-28 12:36:32 +0200

    implement timer

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <Arduino.h>
 
-#include "CubeOS\CubeOS.h"
+#include "CubeOS/CubeOS.h"
+#include "Time/TimeManager.h"
 
 void setup() {
   cubeOS.init();
   demoManager.setBri(100, true);
+  timeManager.init(&demoManager.enableDemo, &demoManager.disableDemo);
 }
 
 void loop() {
