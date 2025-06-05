74d37a94b9472d1b721005a96b8b53e21bfaa95a
Emiel Harmsen <emielch@gmail.com>
2025-06-05 11:43:02 +0200

    update cubeOS

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,24 @@
 #include <Arduino.h>
 
 #include "CubeOS/CubeOS.h"
+#include "CubeOS/Time/TimeManager.h"
+#include "CubeOS/Anim/AnimManager.h"
 #include "Touch/MPR121_Manager.h"
 
 void setup() {
-  cubeOS.init();
   mpr121Manager.init();
-  demoManager.setBri(75, true);
-  timeManager.setOnTime(8.5 * 60 * 60);
-  timeManager.setOffTime(18 * 60 * 60);
+  cubeOS.init();
+
+  animManager.setAnim(2);
+  
+  cubeOS.setBri(75);
+  timeManager.setOnTime(8,30,0);
+  timeManager.setOffTime(180,0,0);
   timeManager.setOnDay(6, false);  // off on saturday
   timeManager.setOnDay(0, false);  // off on saturday
 }
 
 void loop() {
-  cubeOS.update();
   mpr121Manager.update();
+  cubeOS.update();
 }
